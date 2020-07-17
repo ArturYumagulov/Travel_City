9a2bd260308afc5b112da4862c3f9a7b7a2cfee0
Zico13 <45403895+Zico13@users.noreply.github.com>
2020-07-17 10:44:02 +0300

    Update settings.py

--- travel/settings.py
+++ travel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$2_2)^2!yg=!(g_k-*rrrt_%%b7e27(-vyl3ml+6kt9mwc=x40'
+SECRET_KEY = #####
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True   # отображение ошибок  (True в окне браузера)
