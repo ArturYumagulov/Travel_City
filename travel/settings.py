09dbc58d09925c7bac19b74df0223a1340b60830
ArturYumagulov <zico.13288@gmail.com>
2021-03-07 01:05:28 +0300

    create search routes form

--- travel/settings.py
+++ travel/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = #####
+with open("C:\\Users\\yumar\\Documents\\Keys\\dj_travel.txt", "r") as f:
+    SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True   # отображение ошибок  (True в окне браузера)
